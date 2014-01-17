503390ec840b04b23dae7bea138827e05e933a7c
Nate Anderson <theredpea@gmail.com>
2014-01-17 09:48:34 -0700

    Getting heatbar to render

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,61 @@ window.onload = function(){
 };
 
 function showInfo(data, tabletop){
+	var width=400,
+		height=400,
+		margin= {left:20, right:20, top:20, bottom:20},
+		concepts = d3.set(data.map(function(d){return d.concept})).values(),
+		blockHeight = height / concepts.length,
+		coverages = data.map(function(d){return d.coverage}),
+		maxCoverage = d3.max(coverages),
+		minCoverage = d3.min(coverages),
+		colorScale = d3.scale.linear()
+						.domain([minCoverage, maxCoverage])
+						.range(['red', 'green']);
 
-	document.body.innerText=JSON.stringify(data,undefined, true);
+
+
+
+	//TODO: Hook up concepts to a select>options
+	console.log(concepts);
+
+
+
+	window.data=data; //For playing in console
+	
+	var svg= d3.select('body')
+		.append('svg')
+			.attr('width', 400)
+			.attr('height', 400)
+			.append('g')
+				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	var title = svg.append('text')
+				.attr('class', 'title')
+				.attr('dy', '.71em')
+				.text('Concept Heat Map');
+
+	var nestConcepts = d3
+								.nest()
+								.key(function(r){return r.concept}),
+								//.key(function(r){ return r.company}),
+
+		conceptAverage = nestConcepts
+								.rollup(function(a){return d3.mean(a.map(function(c){return c.coverage}))})
+								.entries(data); 
+								//entries returns array {key:{keyValue}, values:{value(s)}}, vs map which returns {{keyValue}:{value(s)}}
+								//The former maintains an order
+		coverages=conceptAverage.map(function(_){return _.values});
+		colorScale.domain([d3.min(coverages), d3.max(coverages)]);
+
+	svg.selectAll('rect.coverage')
+		.data(conceptAverage) //key is the concept, value is the average coverage
+		.enter()
+		.append('rect')
+		.attr('class','coverage')
+		.attr('y', function(d, i){ return i*blockHeight })
+		.attr('x', 0)
+		.attr('width', blockHeight).attr('height', blockHeight)
+		.style('fill', function(d){ return colorScale(d.values); });
 
 }

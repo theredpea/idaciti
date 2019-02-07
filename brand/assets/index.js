1f4bb038b92d62e0d20803a58b0011e070f836df
Nate Anderson <theredpea@gmail.com>
2019-02-06 21:12:10 -0700

    Click signin

--- brand/assets/index.js
+++ brand/assets/index.js
@@ -1,5 +1,99 @@
 // document.onload = function () {
 // https://stackoverflow.com/a/807895/1175496
+// document.onload = function () {
+// https://stackoverflow.com/a/807895/1175496
+
+var animate_loading_ike = function () {
+    // Following point-along-path interpolation example
+    // https://bl.ocks.org/mbostock/1705868
+
+    var perfect_circle = (function () {
+        var points = [
+            [-20, 0],
+            [-14, -14],
+            [0, -20],
+            [14, -14],
+            [20, 0],
+            [14, 14],
+            [0, 20],
+            [-14, 14]
+        ].map(_ => {
+
+            _[0] *= 1.8;
+            _[1] *= 1.8;
+            // Center (?) 
+            _[0] += 27;
+            _[1] += 28;
+            return _;
+        });
+
+        points.push(points.shift());
+        return points;
+
+    })();
+
+    var points = perfect_circle;
+
+    var svg = d3.select("svg#svg1415");
+
+    var path = svg.append("path")
+        .attr('fill', 'transparent')
+        // 2px solid #3b3b3b')
+        .classed('catmull-rom-curve', true)
+        .data([points])
+        .attr("d", d3.line().curve(d3.curveCatmullRomClosed.alpha(0.5)));
+
+
+    points.forEach(function (point, i) {
+
+        var dotty_circle = svg.append("circle")
+            .classed('dotty', true)
+            // The enhanced-secnodar-color
+            .attr('fill', '#3b3b3b')
+            .attr('data-dotty-index', i)
+            .attr("r", 4)
+            .attr('transform', 'translate(' + point[0] + ',' + point[1] + ')')
+
+        // dotty_circle.append('text')
+        //     .text(i)
+        // .attr("transform", "translate(" + points[1] + ")");
+
+        // No delay
+        transition(dotty_circle, 0, i / points.length);
+    });
+
+
+    function transition(circle, delay, frac) {
+        circle.transition()//(1))
+            .delay(delay || 0)
+            .duration(3600)
+            // Maybe easing doesnt happen
+            // .ease(d3.easeQuadOut)
+            .ease(d3.easeLinear)
+            .attrTween("transform", translateAlong(path.node(), frac))
+            .on("end", function () {
+                return transition(circle, delay, frac)
+            });
+    }
+
+    // Returns an attrTween for translating along the specified path element.
+    function translateAlong(path, frac) {
+        var l = path.getTotalLength(); // * (frac || 1);
+        return function (d, i, a) {
+            return function (t) {
+                // var p = path.getPointAtLength(Math.abs(t - frac || 0) * l);
+                var progress = t - frac || 0;
+                // (t - frac < 0 ) ? 
+                progress = progress < 0 ? (progress + 1) : progress;
+                // if (progress<0)
+                var p = path.getPointAtLength(progress * l);
+                return "translate(" + p.x + "," + p.y + ")";
+            };
+        };
+    }
+
+
+}
 
 window.onload = function () {
     // Following point-along-path interpolation example
@@ -239,7 +333,8 @@ window.onload = function () {
     // var points = story_points;
 
 
-    var svg = d3.select("svg");
+    var svg = d3.select('svg#svg3337');
+    // ('[docname="goddess_of_fintech.svg"]');
 
     var path = svg.append("path")
         .attr('fill', 'transparent')
@@ -474,7 +569,7 @@ window.onload = function () {
 
     var scroll_callback = function (event) {
         var scroll = this.scrollY;
-        console.log(scroll)
+        // console.log(scroll)
         d3.selectAll('.header, .highlight-notification-strip').classed('fixed', this.scrollY !== 0)
         // d3.select('.header').toggleClass('fixed', this.scrollY === 0)
         // if (this.scrollY === 0) {
@@ -485,4 +580,21 @@ window.onload = function () {
 
     window.addEventListener("scroll", scroll_callback);
     scroll_callback();
+
+    // document.getElementsByClassName('sign-in')[0].
+    d3.select('.sign-in')
+        .on('click', function () {
+            d3.select('.loading-curtain-drop')
+                .classed('loading', true);
+            // alert('hi');
+        })
+    d3.select('.loading-curtain-drop')
+        .on('click', function () {
+            d3.select('.loading-curtain-drop')
+                .classed('loading', false);
+
+        })
+
+    animate_loading_ike();
 }
+
--- brand/assets/landing.js
+++ brand/assets/landing.js
@@ -1,7 +1,7 @@
 // document.onload = function () {
 // https://stackoverflow.com/a/807895/1175496
 
-window.onload = function () {
+var animate_loading_ike = function () {
     // Following point-along-path interpolation example
     // https://bl.ocks.org/mbostock/1705868
 
@@ -31,8 +31,8 @@ window.onload = function () {
     })();
 
     var points = perfect_circle;
-    
-    var svg = d3.select("svg");
+
+    var svg = d3.select("svg#svg1415");
 
     var path = svg.append("path")
         .attr('fill', 'transparent')
@@ -90,99 +90,7 @@ window.onload = function () {
         };
     }
 
-    var goddess_height = 3;
-    var last_goddess_height = -goddess_height;
-
-    var goddess = d3.select('#g9282')
-        .attr('transform', 'translate(0,' + last_goddess_height + ')');
-
-    goddessHover();
-
-    function goddessHover() {
-        goddess
-            .transition()
-            .duration(2000)
-            .attrTween('transform', function () {
-                return function (t) {
-                    return 'translate(0, ' + (last_goddess_height - (last_goddess_height - goddess_height) * t) + ')';
-                };
-            })
-            .on('end', function () {
-                last_goddess_height = goddess_height;
-                goddess_height = -goddess_height;
-                goddessHover();
-            })
-    }
-
-    var goddess_shadow_scale = goddess_height > 0 ? 0.5 : 1.5;
-    var last_goddess_shadow_scale = -goddess_shadow_scale;
-
-    var goddess_shadow_pulse_factor = 1.3;
-    var last_factor = goddess_height > 0 ? goddess_shadow_pulse_factor : (1 / goddess_shadow_pulse_factor);
-
-    var original_goddess_shadow_length = 20;
-    var goddess_shadow_length = original_goddess_shadow_length;
-    var last_goddess_shadow_length = original_goddess_shadow_length * last_factor;
-
-    var original_goddess_shadow_height = 3.3;
-    var goddess_shadow_height = original_goddess_shadow_height;
-    var last_goddess_shadow_height = original_goddess_shadow_height * last_factor;
-
-    var original_goddess_shadow_opacity = 0.2;
-    var goddess_shadow_opacity = original_goddess_shadow_opacity;
-    var last_goddess_shadow_opacity = original_goddess_shadow_opacity * last_factor;
-
-    var goddess_shadow = d3.select('#path1854-9')
-        .style('fill', 'grey')
-        .attr('rx', original_goddess_shadow_length)
-        .attr('ry', original_goddess_shadow_height)
-        .style('fill-opacity', original_goddess_shadow_opacity)
-        .attr('transform', 'scale(1 1)');
-
-    goddessShadowPulse(2000);
-
-    function goddessShadowPulse(delay) {
-        goddess_shadow
-            .transition()
-            .delay(delay || 0)
-            .duration(2000)
-            .attrTween('rx', function () {
-                return function (t) {
-                    return (last_goddess_shadow_length - (last_goddess_shadow_length - goddess_shadow_length) * t);
-                };
-            })
-            .attrTween('ry', function () {
-                return function (t) {
-                    return (last_goddess_shadow_height - (last_goddess_shadow_height - goddess_shadow_height) * t);
-                };
-            })
-            .styleTween('fill-opacity', function () {
-                return function (t) {
-                    return (last_goddess_shadow_opacity - (last_goddess_shadow_opacity - goddess_shadow_opacity) * t);
-                };
-            })
-            .on('end', function () {
-
-                last_goddess_shadow_length = goddess_shadow_length;
-                last_goddess_shadow_height = goddess_shadow_height;
-                last_goddess_shadow_opacity = goddess_shadow_opacity;
-                //                 goddess_shadow_length
-                // goddess_shadow_height
-                goddess_shadow_length = last_goddess_shadow_length > original_goddess_shadow_length
-                    ? original_goddess_shadow_length / goddess_shadow_pulse_factor
-                    : original_goddess_shadow_length * goddess_shadow_pulse_factor;
-
-
-                goddess_shadow_height = last_goddess_shadow_height > original_goddess_shadow_height
-                    ? original_goddess_shadow_height / goddess_shadow_pulse_factor
-                    : original_goddess_shadow_height * goddess_shadow_pulse_factor;
-
-                goddess_shadow_opacity = last_goddess_shadow_opacity > original_goddess_shadow_opacity
-                    ? original_goddess_shadow_opacity / goddess_shadow_pulse_factor
-                    : original_goddess_shadow_opacity * goddess_shadow_pulse_factor;
-
-                goddessShadowPulse();
-            })
-    }
 
 }
+
+window.onload = animate_loading_ike;
